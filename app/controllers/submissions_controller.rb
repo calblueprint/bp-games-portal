cad6854b497dda362afe0b13ecf9abe9d1c3dd1c
Howard Chen <howardxchen@gmail.com>
2014-09-07 19:22:34 -0700

    Submission now works

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -5,5 +5,17 @@ class SubmissionsController < ApplicationController
   end
 
   def new
+    @submission = Submission.new
+  end
+
+  def create
+    @submission = Submission.create(submission_params)
+    redirect_to new_submission_path
+  end
+
+  private
+
+  def submission_params
+    params.require(:submission).permit(:names, :additional_information)
   end
 end
