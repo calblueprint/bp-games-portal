7543231b22cdd46a140311b93b68bd0ef9a15f5f
Howard Chen <howardxchen@gmail.com>
2014-09-07 18:47:30 -0700

    Add models

--- db/migrate/20140908014302_create_submissions.rb
+++ db/migrate/20140908014302_create_submissions.rb
@@ -1,8 +1,10 @@
-class AddSubmissions < ActiveRecord::Migration
+class CreateSubmissions < ActiveRecord::Migration
   def change
     create_table :submissions do |t|
       t.string :names
       t.text :additional_information
+
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140908012737) do
+ActiveRecord::Schema.define(version: 20140908014302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "submissions", force: true do |t|
-    t.string "names"
-    t.text   "additional_information"
+    t.string   "names"
+    t.text     "additional_information"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
